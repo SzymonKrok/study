3e6ae9d4d7565e267d6dc3a5aa3dbb3256a9c172
Szymon <szymonkrok49@gmail.com>
2021-10-23 10:21:13 +0200

    first commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  newTask: string;
+  tasksDone: Array<string> = [];
+  tasksList: Array<string> = [];
+  tasksRandom: Array<string> = ['Posprzątać mieszkanie', "Wynieść śmieci"];
+  number: number = 0;
+  isClicked: boolean = false;
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+
+    // Trzeba nową funkcję
+
+
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+}
+
+
+
+
+
