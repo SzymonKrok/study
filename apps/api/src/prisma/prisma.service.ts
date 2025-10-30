29794728cfd0ad2360cc8ff315f152fc95ab869d
Szymon <57088551+SzymonKrok@users.noreply.github.com>
2025-10-30 19:10:15 +0100

    Config monorepo and supabase (#1)
    Co-authored-by: Szymon Krok <szymon@MacBook-Pro-Szymon-2.local>

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "../../../../packages/database";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
+  async onModuleInit() {
+    await (this as unknown as { $connect: () => Promise<void> }).$connect();
+  }
 }
